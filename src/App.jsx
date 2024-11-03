de797aee7b4e90a67d715b33c4467899b8dc72ca
neuervinch <npandian515@gmail.com>
2024-11-03 19:09:04 +0530

    2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div  className='text-2xl text-white  underline '>
+      Text ME 
+      
+    </div>
+  )
+}
+
+export default App
+ 
