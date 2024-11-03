56f79a21f731ccecb7fea5b1d48648069c977f45
neuervinch <npandian515@gmail.com>
2024-11-03 21:27:59 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import Hero from './sections/Hero.jsx';
+import About from './sections/About.jsx';
+import Footer from './sections/Footer.jsx';
+import NavBar from './sections/NavBar.jsx';
+import Contact from './sections/Contact.jsx';
+import Clients from './sections/Clients.jsx';
+import Projects from './sections/Projects.jsx';
+import WorkExperience from './sections/Experience.jsx';
 
 const App = () => {
   return (
-    <div  className='text-2xl text-white  underline '>
-      Text ME 
-      
-    </div>
-  )
-}
+    <main className="max-w-7xl mx-auto relative">
+      <NavBar />
+      <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <WorkExperience />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
 
-export default App
- 
+export default App;
